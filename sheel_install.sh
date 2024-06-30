03f2d7ac41f61f64ddabe5346662647b0e9ba18a
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2024-06-30 21:42:33 +0200

    Create sheel_install.sh

---
+++ sheel_install.sh
@@ -0,0 +1,13 @@
+# prepare kernel configuration
+
+make defconfig
+echo 'CONFIG_MODULES=y' >> .config
+echo 'CONFIG_MODULE_UNLOAD=y' >> .config
+echo 'CONFIG_MODULE_FORCE_UNLOAD=y' >> .config
+echo 'CONFIG_DEBUG_KERNEL=y' >> .config
+echo 'CONFIG_DEBUG_INFO=y' >> .config
+echo 'CONFIG_DEBUG_FS=y' >> .config
+echo 'CONFIG_GDB_SCRIPTS=y' >> .config
+echo 'CONFIG_MAGIC_SYSRQ=y' >> .config
+
+make olddefconfig
